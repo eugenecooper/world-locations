4794acf0ec572427f1eef25e8b6e2a5108166791
Eugene Cooper <eugene_cooper@live.com>
2017-01-13 08:07:32 +0200

    Minimal required functionality added

--- src/Locations.php
+++ src/Locations.php
@@ -15,23 +15,74 @@ class Locations
      */
     public function __construct($app_id = null, $app_secret = null)
     {
-        if ( $app_id == null || $app_secret == null ) {
-            $this->vk = new \VK\VK(5818641, 'qJZVN6VtZJuUuog6q7ws');
-        } else {
-            $this->vk = new \VK\VK($app_id, $app_secret);
-        }
+        $this->vk = new \VK\VK($app_id, $app_secret);
     }
 
 
     /**
      * Returns an array containing all the countries in the world
      *
-     * @return mixed
+     * @return array|bool
      */
     public function getCountries()
     {
-        $responce = $this->vk->api('database.getCountries', ['need_all' => 1, 'count' => 1000]);
-        return $responce;
+        $response = $this->vk->api('database.getCountries', ['need_all' => 1, 'count' => 1000]);
+        if (isset($response['response'])) {
+            return $response['response'];
+        }
+        return false;
+    }
+
+
+    /**
+     * Return all cities of specified country
+     *
+     * @param $country_id
+     * @param null $offset
+     * @return bool
+     */
+    public function getCountryCities($country_id, $offset = null)
+    {
+        $response = $this->vk->api('database.getCities', ['country_id' => $country_id, 'need_all' => 1, 'count' => 1000, 'offset' => $offset]);
+        if (isset($response['response'])) {
+            return $response['response'];
+        }
+        return false;
+    }
+
+
+    /**
+     * Return all cities of specified region
+     *
+     * @param $country_id
+     * @param $region_id
+     * @param null $offset
+     * @return bool
+     */
+    public function getRegionCities($country_id, $region_id, $offset = null)
+    {
+        $response = $this->vk->api('database.getCities', ['country_id' => $country_id, 'region_id' => $region_id, 'need_all' => 1, 'count' => 1000, 'offset' => $offset]);
+        if (isset($response['response'])) {
+            return $response['response'];
+        }
+        return false;
+    }
+
+
+    /**
+     * Return all regions of specified country
+     *
+     * @param $country_id
+     * @param null $offset
+     * @return bool
+     */
+    public function getCountryRegions($country_id, $offset = null)
+    {
+        $response = $this->vk->api('database.getRegions', ['country_id' => $country_id, 'need_all' => 1, 'count' => 1000, 'offset' => $offset]);
+        if (isset($response['response'])) {
+            return $response['response'];
+        }
+        return false;
     }
 
 
